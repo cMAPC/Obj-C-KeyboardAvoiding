0004d95971a16246e400fcaa0424dc7aed75b6ef
Marcel <marcel@Marcels-MacBook-Pro.local>
2017-03-22 20:06:44 +0300

    Initial Commit

---
+++ PopUpView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopUpView
+//
+//  Created by Marcel  on 3/22/17.
+//  Copyright © 2017 Marcel . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PopUpView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PopUpView
+//
+//  Created by Marcel  on 3/22/17.
+//  Copyright © 2017 Marcel . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
